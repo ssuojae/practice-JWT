cb18c4ab2e398542d573dea575a065b467499339
suojae <suojae.developer@gmail.com>
2024-09-21 18:47:06 +0900

    fix: PostgreSQL 연결 문제 해결
    postgreSQL 연결 문제는 127.0.0.1 대신 nestjs_postgres를 사용하여 해결!

--- practice-jwt-back/src/config/database.config.ts
+++ practice-jwt-back/src/config/database.config.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.POSTGRES_USER, // 환경변수로부터 유저명 받음
-  password: process.env.POSTGRES_PASSWORD, // 환경변수로부터 비밀번호 받음
-  database: process.env.POSTGRES_DB, // 환경변수로부터 DB 이름 받음
+  host: 'nestjs_postgres',
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true, // 개발 환경에서만 true
+  synchronize: true,
 };
