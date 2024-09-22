a2169787bd333b3e33fdbdaec9d1e8f0c458f09b
suojae <suojae.developer@gmail.com>
2024-09-22 20:59:04 +0900

    chore: jwt secret 외부에서 주입

--- practice-jwt-back/src/auth/jwt.service.ts
+++ practice-jwt-back/src/auth/jwt.service.ts
@@ -1,26 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
-import { UserEntity } from '../user/user.entity';
 import { IJwtService } from './interfaces/jwt.service.interface';
+import { UserEntity } from '../user/user.entity';
 
 @Injectable()
 export class JwtService implements IJwtService {
+  constructor(@Inject('JWT_SECRET') private readonly jwtSecret: string) {}
+
   signToken(user: UserEntity) {
     const payload = { id: user.id, email: user.email };
-    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
-    const refreshToken = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '7d',
-    });
+    const accessToken = jwt.sign(payload, this.jwtSecret, { expiresIn: '1h' });
+    const refreshToken = jwt.sign(payload, this.jwtSecret, { expiresIn: '7d' });
 
     return { accessToken, refreshToken };
   }
 
   verifyToken(token: string) {
     try {
-      return jwt.verify(token, process.env.JWT_SECRET);
-    } catch {
+      return jwt.verify(token, this.jwtSecret);
+    } catch (error) {
+      console.error('Token verification error:', error);
       return null;
     }
   }
