95c973e8d8f27e624dc71cc81ff1280d466742eb
suojae <suojae.developer@gmail.com>
2024-09-22 21:04:56 +0900

    chore: app module 수정

--- practice-jwt-back/src/app.module.ts
+++ practice-jwt-back/src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { typeOrmConfig } from './config/database.config';
-import { RedisService } from 'nestjs-redis';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // 환경 변수를 전역에서 사용 가능하게 설정
+      isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeOrmConfig), // PostgreSQL 설정
+    AuthModule,
   ],
-  providers: [RedisService], // Redis 서비스 등록
 })
 export class AppModule {}
