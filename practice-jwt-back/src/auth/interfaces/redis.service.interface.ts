c2888edc5786c717f5eaea6b3da71835ed8c36bc
suojae <suojae.developer@gmail.com>
2024-09-22 20:58:16 +0900

    fix: redis 에 config 안넣은 에러 수정

--- practice-jwt-back/src/auth/interfaces/redis.service.interface.ts
+++ practice-jwt-back/src/auth/interfaces/redis.service.interface.ts
@@ -1,5 +1,4 @@
 export abstract class IRedisService {
   abstract storeRefreshToken(userId: string, token: string): Promise<void>;
   abstract getRefreshToken(userId: string): Promise<string>;
-  abstract invalidateToken(userId: string): Promise<void>;
 }
--- practice-jwt-back/src/auth/redis.service.ts
+++ practice-jwt-back/src/auth/redis.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import Redis from 'ioredis';
+import { Redis } from 'ioredis';
 import { IRedisService } from './interfaces/redis.service.interface';
+import { redisConfig } from '../config/redis.config'; // redisConfig 불러오기
 
 @Injectable()
 export class RedisService implements IRedisService {
-  private readonly redisClient = new Redis();
+  private redisClient: Redis;
+
+  constructor() {
+    this.redisClient = new Redis(redisConfig); // redisConfig 사용하여 Redis 클라이언트 생성
+  }
 
   async storeRefreshToken(userId: string, token: string): Promise<void> {
-    await this.redisClient.set(userId, token);
+    await this.redisClient.set(token, userId); // 토큰을 키로, 사용자 ID를 값으로 저장
   }
 
   async getRefreshToken(userId: string): Promise<string> {
