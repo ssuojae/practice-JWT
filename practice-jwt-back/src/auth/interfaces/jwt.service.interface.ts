25e63e52cc99fb3bbdee01f5516e9543823e6c20
suojae <suojae.developer@gmail.com>
2024-09-22 21:03:41 +0900

    chore: 안쓰고있는 함수 삭제

--- practice-jwt-back/src/auth/interfaces/jwt.service.interface.ts
+++ practice-jwt-back/src/auth/interfaces/jwt.service.interface.ts
@@ -1,6 +1,8 @@
 import { UserEntity } from '../../user/user.entity';
 
 export abstract class IJwtService {
-  abstract signToken(user: UserEntity): { accessToken: string, refreshToken: string };
-  abstract verifyToken(token: string): any;
+  abstract signToken(user: UserEntity): {
+    accessToken: string;
+    refreshToken: string;
+  };
 }
