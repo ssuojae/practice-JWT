cbe7868a1c54c6e8fa6b2f747000b1964af0b596
suojae <suojae.developer@gmail.com>
2024-09-30 22:42:51 +0900

    feat: mapper 객체 수정 및 상대 경로로 변경

--- practice-jwt-front-flutter/lib/data/mapper/user_mapper.dart
+++ practice-jwt-front-flutter/lib/data/mapper/user_mapper.dart
@@ -1,12 +1,38 @@
-import 'package:portfolio/data/dto/user_dto.dart';
-import 'package:portfolio/domain/user_entity.dart';
+import '../dto/user_dto.dart';
+import '../../domain/user_entity.dart';
+import '../dao/user_dao.dart';
 
 final class UserMapper {
+
+  // DTO -> Entity
   static UserEntity toEntityFromDTO(UserDTO dto) {
-    return UserEntity(email: dto.email, password: dto.password);
+    return UserEntity(
+      email: dto.email,
+      password: dto.password,
+    );
   }
 
+  // Entity -> DTO
   static UserDTO toDTOFromEntity(UserEntity entity) {
-    return UserDTO(email: entity.email, password: entity.password);
+    return UserDTO(
+        email: entity.email,
+        password: entity.password,
+    );
+  }
+
+  // DAO -> Entity
+  static UserEntity toEntityFromDAO(UserDAO dao) {
+    return UserEntity(
+      email: dao.email,
+      password: dao.password,
+    );
+  }
+
+  // Entity -> DAO
+  static UserDAO toDAOFromEntity(UserEntity entity) {
+    return UserDAO(
+      email: entity.email,
+      password: entity.password,
+    );
   }
 }
