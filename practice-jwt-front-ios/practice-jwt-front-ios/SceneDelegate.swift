34f777a371c25f83f30e978c70786bc82287d0ba
suojae <suojae.developer@gmail.com>
2024-10-01 16:46:56 +0900

    setup: ios 프로젝트 재셋팅

---
+++ practice-jwt-front-ios/practice-jwt-front-ios/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  practice-jwt-front-ios
+//
+//  Created by suojae on 10/1/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ practice-jwt-front-ios/practice-jwt-front-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice-jwt-front-ios
+//
+//  Created by suojae on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
