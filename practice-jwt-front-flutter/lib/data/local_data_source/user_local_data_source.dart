9efe6fb4ea5b97bc0b35e2eee20b673f2f43d137
suojae <suojae.developer@gmail.com>
2024-10-01 12:17:35 +0900

    feat: local 저장 서비스 객체 추가

---
+++ practice-jwt-front-flutter/lib/data/local_data_source/user_local_data_source.dart
@@ -0,0 +1,23 @@
+import 'package:portfolio/data/dao/user_dao.dart';
+import 'package:portfolio/data/local_data_source/secure_token_service.dart';
+
+final class UserLocalDataSource {
+  final SecureTokenService _secureTokenService;
+
+  UserLocalDataSource(this._secureTokenService);
+
+  // 유저 정보를 로컬 저장소에 저장하는 메서드
+  Future<void> saveUserOnfo(UserDAO userDAO) async {
+    await _secureTokenService.saveAccessToken(userDAO.uuid);
+  }
+
+  // 로컬에서 유저 정보 불러오기
+  Future<String?> getUserInfo() async {
+    return await _secureTokenService.getAccessToken();
+  }
+
+  // 로그아웃 시 로컬 정보 삭제
+  Future<void> clearUserInfo() async {
+    await _secureTokenService.deleteAllToken();
+  }
+}
